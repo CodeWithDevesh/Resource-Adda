02d95856374d2d378d4d7e09cfc58f60e7c974f4
CodeWithDevesh <deveshagarwal652005@gmail.com>
2024-09-28 10:58:37 +0530

    added default

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {  useParams } from 'react-router-dom'
-export function Resources() {
+export default function Resources() {
 const {branch: branch ,sem:sem}= useParams();
 console.log(branch,sem)
   return (

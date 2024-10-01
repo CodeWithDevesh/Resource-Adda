dc1bf37a1bc6500770b5d9d78f7b6957485a1673
Devesh Agarwal <deveshagarwal652005@gmail.com>
2024-10-01 23:16:23 +0530

    Fixed css

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -4,13 +4,13 @@ import hb from "../assets/404.gif"
 
 export default function Page404() {
   return (
-    <div class="home">
-      <div class="container">
-        <div class="section-1">
-            <div class="topper-sec">
-                <img src={hb} alt="404 Gif" class="gif-img"/>
-                <div class="text">Bhai tu bhatak gaya hai</div>
-                <a href="" class="btn res-btn">Wapas Jaa</a>
+    <div class="home-404">
+      <div class="container-404">
+        <div class="section-1-404">
+            <div class="topper-sec-404">
+                <img src={hb} alt="404 Gif" class="gif-img-404"/>
+                <div class="text-404">Bhai tu bhatak gaya hai</div>
+                <a href="/" class="btn res-btn">Wapas Jaa</a>
             </div>
         </div>
     </div>
--- frontend/src/pages/SuperAdmin.jsx
+++ frontend/src/pages/SuperAdmin.jsx
@@ -8,7 +8,7 @@ export default function SuperAdmin() {
         <div class="res-inner-super">
   
             <div>
-                <label for="superadmin-password" class="text">Super Admin Password</label>
+                <label for="superadmin-password" class="text-super">Super Admin Password</label>
                 <div class="input-container">
                     <input type="password" id="superadmin-password" class="admin-input password-field" placeholder="Enter Super Admin Password"/>
                 </div>
@@ -16,12 +16,12 @@ export default function SuperAdmin() {
 
 
             <div>
-                <label for="admin-username" class="text">Admin Username</label>
+                <label for="admin-username" class="text-super">Admin Username</label>
                 <input type="text" id="admin-username" class="admin-input" placeholder="Enter Admin Username"/>
             </div>
 
             <div>
-                <label for="admin-password" class="text">Admin Password</label>
+                <label for="admin-password" class="text-super">Admin Password</label>
                 <div class="input-container">
                     <input type="password" id="admin-password" class="admin-input password-field" placeholder="Enter Admin Password"/>
                 </div>

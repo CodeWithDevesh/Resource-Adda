5d4e1adbfc9d5b9bf6a5b918eb1a1d5d64d6010b
akshatabakre <akshatabakre@gmail.com>
2024-10-01 21:06:22 +0530

    updated 404 page

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Page404.css"
+import hb from "../assets/404.gif"
 
 export default function Page404() {
   return (
-    <div>
-      404 Not Found
+    <div class="home">
+      <div class="container">
+        <div class="section-1">
+            <div class="topper-sec">
+                <img src={hb} alt="404 Gif" class="gif-img"/>
+                <div class="text">Bhai tu bhatak gaya hai</div>
+                <a href="" class="btn res-btn">Wapas Jaa</a>
+            </div>
+        </div>
+    </div>
     </div>
   )
 }

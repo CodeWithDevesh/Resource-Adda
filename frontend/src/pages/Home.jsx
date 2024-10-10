27fc90a97b4e5c531a8e9377ea713c744ebed503
Devesh Agarwal <deveshagarwal652005@gmail.com>
2024-10-10 17:07:29 +0530

    Fixed home and resources

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Home.css";
 import NavigateButton from "../components/NavigateButton";
-import character from "../assets/character.png"
-import {motion } from "framer-motion";
+import character from "../assets/character.png";
+import { motion } from "framer-motion";
 export function Home() {
     const TRANSITION_DELAY = 0.5;
     const TRANSITION_DURATION = 1;
@@ -12,36 +12,36 @@ export function Home() {
         <>
             <div className="home">
                 <div className="container">
-                    <motion.div 
-                    initial={{opacity:0}}
-                    animate={{opacity:1}}
-                    exit={{
-                        opacity: 0,
-                        transition: { delay: 0, duration: EXIT_DURATION },
-                    }}
-                    transition={{
-                        delay: TRANSITION_DELAY,
-                        duration: TRANSITION_DURATION,
-                        ease: TRANSITION_TYPE,
-                    }}
-                    className="section-1"
-                    >
-                        <motion.div 
-                        className="slangs"
-                        initial={{ y: -50 }}
-                        animate={{ y: 0 }}
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
                         exit={{
-                            y: -50,
-                            transition: {
-                                delay: 0,
-                                duration: EXIT_DURATION,
-                            },
+                            opacity: 0,
+                            transition: { delay: 0, duration: EXIT_DURATION },
                         }}
                         transition={{
                             delay: TRANSITION_DELAY,
                             duration: TRANSITION_DURATION,
                             ease: TRANSITION_TYPE,
                         }}
+                        className="section-1"
+                    >
+                        <motion.div
+                            className="slangs"
+                            initial={{ y: -50 }}
+                            animate={{ y: 0 }}
+                            exit={{
+                                y: -50,
+                                transition: {
+                                    delay: 0,
+                                    duration: EXIT_DURATION,
+                                },
+                            }}
+                            transition={{
+                                delay: TRANSITION_DELAY,
+                                duration: TRANSITION_DURATION,
+                                ease: TRANSITION_TYPE,
+                            }}
                         >
                             <div className="text first-slang">
                                 <span>Haa bhai... Aa gaya padhne</span>
@@ -51,20 +51,20 @@ export function Home() {
                             </div>
                         </motion.div>
                         <motion.div
-                        initial={{opacity:0, x:100}}
-                        animate={{opacity:1, x:0 }}
-                        transition={{duration:1}}
+                            initial={{ opacity: 0, x: 100 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 1 }}
                         >
-                        <NavigateButton 
-
-                        className="res-btn"
-                         text={"Get Study Material"}
-                          path={"/resources"}/>
+                            <NavigateButton
+                                className="res-btn"
+                                text={"Get Study Material"}
+                                path={"/resources"}
+                            />
                         </motion.div>
                     </motion.div>
-                    <motion.div  
-                    className="topper-sec"
-                    initial={{ opacity: 0 }}
+                    <motion.div
+                        className="topper-sec"
+                        initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{
                             opacity: 0,
@@ -76,9 +76,9 @@ export function Home() {
                             ease: TRANSITION_TYPE,
                         }}
                     >
-                        <motion.span 
-                        className="text"
-                        initial={{ x: -50 }}
+                        <motion.span
+                            className="text"
+                            initial={{ x: -50 }}
                             animate={{ x: 0 }}
                             exit={{
                                 x: -50,
@@ -92,17 +92,20 @@ export function Home() {
                                 duration: TRANSITION_DURATION,
                                 ease: TRANSITION_TYPE,
                             }}
-                        >Topper's Section</motion.span>
-                        <motion.div
-                        initial={{opacity:0, y:100}}
-                        animate={{opacity:1, y:0}}
-                        exit={{opacity:0}}
-                        transition={{duration:1}}
                         >
-                        <NavigateButton 
-                        className={"contri-btn"}
-                         text={"Contribute"}
-                          path={"/contribute"}/>
+                            Topper's Section
+                        </motion.span>
+                        <motion.div
+                            initial={{ opacity: 0, y: 100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 1 }}
+                        >
+                            <NavigateButton
+                                className={"contri-btn"}
+                                text={"Contribute"}
+                                path={"/contribute"}
+                            />
                         </motion.div>
                     </motion.div>
                 </div>

ce8afa7c2dc1024f6ebda2f52dad6bb2c9e5a23f
Devesh Agarwal <deveshagarwal652005@gmail.com>
2024-10-11 13:33:23 +0530

    Added name and email to about us

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,7 +10,12 @@ export default function About() {
     const [selectedId, setId] = useState(null);
 
     return (
-        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1, delay: 1}} className="aboutus">
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="aboutus"
+        >
             <section className="about-sec1 about-sec">
                 <span className="contact-head">About Us</span>
                 <div className="res-inner-contact">
@@ -91,15 +96,24 @@ export default function About() {
                         className="team-mate akshata"
                     >
                         <img src={akshata} alt="" />
-                        <p>
-                            Hey peers! I worked on the design and frontend of
-                            this website, which aims to provide a convenient
-                            platform for you to browse relevant study material
-                            for your semester exams.📚 Leave your worries to us
-                            as the most of these resources are tried and tested,
-                            and have helped us score good. I hope you make the
-                            most of it!🙃
-                        </p>
+                        <div className="team-text">
+                            <h2>Akshata Bakre</h2>
+                            <p>
+                                Hey peers! I worked on the design and frontend
+                                of this website, which aims to provide a
+                                convenient platform for you to browse relevant
+                                study material for your semester exams.📚 Leave
+                                your worries to us as the most of these
+                                resources are tried and tested, and have helped
+                                us score good. I hope you make the most of it!🙃
+                            </p>
+                            <p>
+                                Email:{" "}
+                                <a href="mailto:akshatabakre@gmail.com">
+                                    akshatabakre@gmail.com
+                                </a>
+                            </p>
+                        </div>
                     </motion.div>
                 </div>
             )}
@@ -115,14 +129,24 @@ export default function About() {
                         className="team-mate devesh"
                     >
                         <img src={devesh} alt="" />
-                        <p>
-                            As the lead developer of this website, I’ve worked
-                            on both the frontend and backend. Honestly, my CPI
-                            and I don’t exactly go hand in hand 🤧, which is why
-                            I wanted to create this platform to help others
-                            improve their's. Hopefully, this makes your academic
-                            journey a little easier!😉
-                        </p>
+                        <div className="team-text">
+                            <h2>Devesh Agarwal</h2>
+                            <p>
+                                As the lead developer of this website, I’ve
+                                worked on both the frontend and backend.
+                                Honestly, my CPI and I don’t exactly go hand in
+                                hand 🤧, which is why I wanted to create this
+                                platform to help others improve their's.
+                                Hopefully, this makes your academic journey a
+                                little easier!😉
+                            </p>
+                            <p>
+                                Email:{" "}
+                                <a href="mailto:deveshagarwal652005@gmail.com">
+                                    deveshagarwal652005@gmail.com
+                                </a>
+                            </p>
+                        </div>
                     </motion.div>
                 </div>
             )}
@@ -138,16 +162,22 @@ export default function About() {
                         className="team-mate divyansh"
                     >
                         <img src={divyansh} alt="" />
-                        <p>
-                            As part of the project, I worked on developing a
-                            website where I contributed to making it responsive
-                            across different devices. I was involved in both
-                            designing and creating webpages, ensuring the
-                            website was not only visually appealing but also
-                            fully functional. This project allowed me to enhance
-                            my skills in web design, responsiveness, and
-                            functionality.
-                        </p>
+                        <div className="team-text">
+                            <h2>Divyansh Sharma</h2>
+                            <p>
+                                I developed a responsive website, contributing
+                                to both design and functionality ⚙️. I ensured
+                                it looked great across all devices 📱 and worked
+                                smoothly. This project boosted my skills in web
+                                design, responsiveness, and functionality 🚀.
+                            </p>
+                            <p>
+                                Email:{" "}
+                                <a href="mailto:divyanshsharma35403@gmail.com">
+                                    divyanshsharma35403@gmail.com
+                                </a>
+                            </p>
+                        </div>
                     </motion.div>
                 </div>
             )}
@@ -163,14 +193,24 @@ export default function About() {
                         className="team-mate akshat"
                     >
                         <img src={akshat} alt="" />
-                        <p>
-                            Hello fellow learner! I’m a frontend developer who
-                            came up with the idea for this website and worked
-                            closely with my team to build it.👨🏻‍💻 I focused on
-                            making the site easy to use and visually appealing,
-                            bringing together design and development to turn our
-                            ideas into a working website.🙃
-                        </p>
+                        <div className="team-text">
+                            <h2>Akshat Mishra</h2>
+                            <p>
+                                Hello fellow learner! I’m a frontend developer
+                                who came up with the idea for this website and
+                                worked closely with my team to build it.👨🏻‍💻 I
+                                focused on making the site easy to use and
+                                visually appealing, bringing together design and
+                                development to turn our ideas into a working
+                                website.🙃
+                            </p>
+                            <p>
+                                Email:{" "}
+                                <a href="mailto:akshatm985@gmail.com">
+                                    akshatm985@gmail.com
+                                </a>
+                            </p>
+                        </div>
                     </motion.div>
                 </div>
             )}
